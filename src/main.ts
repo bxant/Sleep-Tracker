7d6e38a3d98017826e824c9ab76a7a42f75d1b97
bxant <bryantflores54327@gmail.com>
2021-01-12 16:56:22 -0800

    added project base

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
