c3ab08c28f67de1f900ff3d0438e89aa0ed1a178
bxant <bryantflores54327@gmail.com>
2021-01-16 20:48:59 -0800

    updated TODO tab2.page.ts
    added todo template for future development

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,13 @@ export class Tab2Page {
 
   constructor() {}
 
+  // TODO: Need to bind the months to array that can be used
+  // to filter through our sleep data.
+  // Need to also bind days to array to use for filtering of data.
+
+  //TODO: need to be able to also bind both days and months to our information.
+  // probably use a boolean value to ensure that our days are
+  // actually "filterable" <-- not sure if that's a word
+  // but I hope you get the idea.
+
 }
