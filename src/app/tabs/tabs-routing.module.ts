42bbe5fb320e6ebe1518efcbc43d487fbc36d77c
bxant <bryantflores54327@gmail.com>
2021-01-17 19:47:02 -0800

    added profile page
    added another module page for user profile. Currently empty

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
+
+
+
 const routes: Routes = [
   {
     path: 'tabs',
@@ -18,6 +21,10 @@ const routes: Routes = [
       {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
       },
       {
         path: '',
