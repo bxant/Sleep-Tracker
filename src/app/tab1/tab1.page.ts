55ae263d8a8635900d8f107e32a757337c5f1dc7
Trevor Keller <tlkeller@uci.edu>
2021-02-02 21:28:01 -0800

    Further Theming and Styling

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,7 @@ import { ToastController } from '@ionic/angular';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { SleepData } from '../data/sleep-data';
 import { LogsleepService } from '../services/logsleep.service';
+import { SleepService } from '../services/sleep.service';
 
 // Ionic Storage
 import { Storage } from '@ionic/storage';
@@ -17,78 +18,57 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page{
-
-  
   // constructor has toastController to ensure we can
   // notify user when they press buttons/do certain actions.
-  constructor(public toastController: ToastController, public sleepService: LogsleepService,
-    public storage:Storage) {
-    // this.sleepService = SleepService;
+  constructor(public toastController: ToastController, 
+    public storage :Storage, private sleepService: SleepService) {
   }
 
-  // sleepStart is when user logs sleep
+  // Time/Date of when the user started and ended their sleep
   private sleepStart:Date;
-  // sleepEnd is when user logs when they wake up
   private sleepEnd:Date;
 
-  // when the user inputs a different time on screen
-  // this will turn to true
-  private userLoggedStartSleep:boolean;
-  private userLoggedEndSleep:boolean;
-
-  
-  get allSleepData() {
-		return LogsleepService.AllSleepData;
-	}
-
-  userEnteredSleepStart()
+  // Store sleep data with SleepService
+  async logSleep()
   {
-    // user interacted with timer button here also.
-    // for: sleepStart
-    this.userLoggedStartSleep = true;
-    
-  }
-
-  userEnteredSleepEnd()
-  {
-    // means that user messed with the timer button
-    // for: sleepEnd
-    this.userLoggedEndSleep = true;
-  }
-
-  logSleep()
-  {
-    // just a placeholder function, will later be added to storage/array
-    // for data collection
-
-    // console for debugging
-    // console.log(this.sleepStart);
-    // console.log(this.sleepEnd);
-
-    // this condition checked if user
-    // actually changed the times from when they logged sleep.
-    // will notify user if it is missing information using toast
-    // notification.
-    if (this.userLoggedEndSleep == true && this.userLoggedStartSleep == true){
-			this.toastController.create({
-				message: 'Sleep Logged Successfully!',
-        duration: 2500,
-        position: "top"		
-				}).then((toast) => {
-        toast.present();
-        
-        console.log("Sleep DATA should appear");
-        var toLog = new OvernightSleepData(this.sleepStart, this.sleepEnd);
-        this.sleepService.logOvernightData(toLog);
-        this.storage.set("overnightSleep", toLog);
-        console.log(this.allSleepData);
-				});
-			}
+    // If times are entered, log data and give success notif
+    if (this.sleepStart != undefined && this.sleepEnd != undefined){
+      var data = new OvernightSleepData(this.sleepStart, this.sleepEnd);
+      this.sleepService.addToStorage(data);
+      const add_toast = await this.toastController.create(
+      {
+        message: "Sleep Data Logged",
+        duration: 3000,
+        buttons: [
+          {
+            text: "Undo",
+            handler: () => {
+              this.sleepService.deleteFromStorage(data.id);
+            }
+          }
+        ]
+      });
+      add_toast.present();
+      
+      // this.toastController.create({
+      //   message: 'Sleep Logged Successfully!',
+      //   duration: 2500,
+      //   position: "top"		
+      // }).then((toast) => {
+      //   toast.present();
+          
+      //   console.log("Sleep DATA should appear");
+      //   var toLog = new OvernightSleepData(this.sleepStart, this.sleepEnd);
+      //   this.sleepService.logOvernightData(toLog);
+      //   this.storage.set("overnightSleep", toLog);
+      //   console.log(this.allSleepData);
+      // });
+    }
 		else{
 			this.toastController.create({
-				message: 'Missing sleep information',
-				duration: 2500,
-				position: "top"		
+				message: 'Missing Sleep Information',
+				duration: 3000,
+        position: "top",	
 				}).then((toast) => {
 				toast.present();
 				});
