20a678dda054dccca826c603de2a1df458007535
bxant <bryantflores54327@gmail.com>
2021-02-07 22:38:53 -0800

    added profile stuff for 2/7/21 checkpoint
    the journey of a thousand steps begins with the first one

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -9,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  private firstName:string;
+  private lastName:string;
   constructor() { }
+
+   
+
+
+
+  firstNameChanged()
+  {
+
+    console.log("firstName changed");
+    console.log(this.firstName);
+    console.log("should have printed name");  }
+
+  lastNameChanged()
+  {
+  
+    console.log("printing Last name");
+    
+    console.log(this.lastName);
+    console.log("should have printed last name");  
+  }
 
   ngOnInit() {
   }
