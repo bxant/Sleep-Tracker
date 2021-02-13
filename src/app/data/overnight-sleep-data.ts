9ea38785d5f2dbe1fb43c010c6928e3fbec723d0
bxant <bryantflores54327@gmail.com>
2021-02-13 14:32:16 -0800

    added hours slept attr
    now able to determine if user has a certain amount of hours slept

--- src/app/data/overnight-sleep-data.ts
+++ src/app/data/overnight-sleep-data.ts
@@ -4,12 +4,20 @@ import { SleepData } from './sleep-data';
 export class OvernightSleepData extends SleepData {
 	private sleepStart:Date;
 	private sleepEnd:Date;
+	private hoursSlept:number;
 
 	constructor(sleepStart:Date, sleepEnd:Date, id:string = generate()) {
 		super();
 		this.sleepStart = sleepStart;
 		this.sleepEnd = sleepEnd;
 		this.id = id;
+		var sleepStart_ms = this.sleepStart.getTime();
+		var sleepEnd_ms = this.sleepEnd.getTime();
+
+		// Calculate the difference in milliseconds
+		var difference_ms = sleepEnd_ms - sleepStart_ms;
+
+		this.hoursSlept = Math.floor(difference_ms / ( 1000*60*60));
 	}
 
 	summaryString():string {
@@ -18,7 +26,9 @@ export class OvernightSleepData extends SleepData {
 
 		// Calculate the difference in milliseconds
 		var difference_ms = sleepEnd_ms - sleepStart_ms;
-		    
+
+		this.hoursSlept = Math.floor(difference_ms / ( 1000*60*60));
+		
 		// Convert to hours and minutes
 		return "Time Slept: " + Math.floor(difference_ms / (1000*60*60)) + " hours, " + Math.floor(difference_ms / (1000*60) % 60) + " minutes";
 	}
