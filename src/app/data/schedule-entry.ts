70d54b021954eaf5ad7c0200933a860cc4a5eccd
bxant <bryantflores54327@gmail.com>
2021-02-16 11:10:52 -0800

    added schedule entry class to master
    going to mess around with schedule thing for profile

--- src/app/data/schedule-entry.ts
+++ src/app/data/schedule-entry.ts
@@ -1,2 +1,11 @@
-export interface ScheduleEntry {
+
+
+import { generate } from 'shortid';
+import { SleepData } from './sleep-data';
+
+
+export class ScheduleEntry extends SleepData{
+
+    private dayStarts:Date;
+    private dayEnds:Date;
 }
