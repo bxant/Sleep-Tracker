3463efba25638fc9320eb67e0887409e36b23021
Trevor Keller <tlkeller@uci.edu>
2021-02-18 16:18:11 -0800

    Fixed user info on profile tab

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -25,59 +25,23 @@ export class ProfilePage implements OnInit {
   private firstName:string;
   private lastName:string;
   private userAge:number;
+  
   constructor(public toastController: ToastController, 
-  public storage :Storage, public sleepService:SleepService) {
+              public storage :Storage, public sleepService:SleepService) {
 
-    
-    
    }
 
-   
-
-
-
-  firstNameChanged()
-  {
-    
-    console.log("firstName changed");
-    console.log(this.firstName);
-    console.log("should have printed name");  }
-
-  lastNameChanged()
-  {
-  
-    console.log("printing Last name");
-    
-    console.log(this.lastName);
-    console.log("should have printed last name");  
-  }
-
-  updateAge()
-  {
-    console.log(this.userAge);
-
-  }
-
   ngOnInit() {
-    // this.firstName = "Peter";
-    // this.lastName = "Anteater";
-    // this.userAge = 22;
-    this.storage.get("userAge");
-    this.storage.get("userFirstName");
-    this.storage.get("userLastName");
-    
+    this.storage.get("userFirstName").then((data) => {this.firstName = data});
+    this.storage.get("userLastName").then((data) => {this.lastName = data});
+    this.storage.get("userAge").then((data) => {this.userAge = data});
   }
 
   async updateUserInfo()
   {
-    
     this.storage.set("userAge", this.userAge);
     this.storage.set("userFirstName", this.firstName);
     this.storage.set("userLastName", this.lastName);
-
-    this.storage.get("userAge");
-    this.storage.get("userFirstName");
-    this.storage.get("userLastName");
     const add_toast = await this.toastController.create(
       {
         message: "Updated User Info!",
